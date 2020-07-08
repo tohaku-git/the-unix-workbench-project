05ba07c4355ce905c0b949be310d4b26c474ed52
tohaku-git <67587078+tohaku-git@users.noreply.github.com>
2020-07-08 23:33:18 +0900

    modified the program

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,16 +1,19 @@
 file_num=$(ls -1 | wc -l)
 
 function response {
-	if [[ $1 -eq $2 ]]
+	if [[ $1 =~ [^0-9] ]]
 	then
-		echo "You entered: $1, that is equal to the actual number of files. You have completed this game."
+		echo "You entered: $1, that is not an integer. Please enter an integer."
+	elif [[ $1 -eq $2 ]]
+	then
+		echo "You entered: $1, that is equal to the actual number of files. You have completed this game. Congratulations!"
 		let $3=1
 	elif [[ $1 -gt $2 ]]
 	then
-		echo "You entered: $1, that is bigger than the actual number of files. Please try again."
+		echo "You entered: $1, that is higher than the actual number of files. Please try again."
 	elif [[ $1 -lt $2 ]]
 	then
-		echo "You entered: $1, that is smaller than the actual number of files. Please try again."
+		echo "You entered: $1, that is lower than the actual number of files. Please try again."
 	fi
 }
 
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 #Assignment of the Unix Workbench
 The date and time at which make was run:
-Tue Jul  7 23:52:06 JST 2020
+Wed Jul  8 23:32:27 JST 2020
 The number of lines contained in guessinggame.sh:
-24
+27
